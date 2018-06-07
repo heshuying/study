62b5b26b3f8dcad97eff4eea0e6665543ff1bb4b
eyesfreefly <33392757+eyesfreefly@users.noreply.github.com>
2018-06-07 10:57:27 +0800

    account

---
+++ AccountTrade.java
@@ -0,0 +1,87 @@
+package com.hyjf.mybatis.model.auto;
+
+import java.io.Serializable;
+
+public class AccountTrade implements Serializable {
+    private Short id;
+
+    private Integer typeId;
+
+    private String name;
+
+    private String nid;
+
+    private String value;
+
+    private Integer status;
+
+    private Short order;
+
+    private String operation;
+
+    private static final long serialVersionUID = 1L;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid == null ? null : nid.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Short getOrder() {
+        return order;
+    }
+
+    public void setOrder(Short order) {
+        this.order = order;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation == null ? null : operation.trim();
+    }
+}
